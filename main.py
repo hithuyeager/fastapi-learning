0c75a50dc4a56f73c6bbe7f1fcae16ccd3399c10
hithesh <hitheshrhithu41@gmail.com>
2026-04-06 14:41:55 +0000

    testing 1

--- main.py
+++ main.py
@@ -1,17 +1,2 @@
-from pydantic import BaseModel
-from fastapi import FastAPI 
-
-app = FastAPI()
-
-class User(BaseModel):
-    username: str
-    age: int
-
-db = {}
-
-@app.post("/")
-async def create_user(user: User):
-    if user.username in db:
-        return {"error": "Username already exists"}
-    db[user.username] = user
-    return {"message": "User created successfully", "user": user} 
+print ("hello world")
+print ("welcome to python programming")
