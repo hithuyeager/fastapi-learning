b8cba37c35e045aee161f2d41fb42dac0a3d3b5b
hithesh <hitheshrhithu41@gmail.com>
2026-04-06 15:20:25 +0000

    implemented small api

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print ("hello world")
-print ("welcome to python programming")
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class User(BaseModel):
+    username: str
+
+@app.post("/users/")
+async def create_user(user: User):
+    return {"username": user.username}
